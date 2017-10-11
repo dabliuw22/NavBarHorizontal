2cd6377ad2463d85f92f6ff55dfceaaa206c9cc3
dabliuw22 <wleylop22@gmail.com>
2017-10-11 15:06:28 -0500

    Agregando funcionalidad responsive del menú

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-	
+	$(window).resize(function() {
+		if($(document).width() > 768) {
+			$(".nav-bar").css({'display': 'block'});
+		} else {
+			$(".nav-bar").css({'display': 'none'});
+		}
+		$(".nav-bar").removeClass("activo");
+	});
+
+	$("#btn-menu-select").click(function() {
+		if($(".nav-bar").hasClass("activo")) {
+			//$(".nav-bar").slideUp();
+			$(".nav-bar").css({'display': 'none'});
+			$(".nav-bar").removeClass("activo");
+		} else {
+			//$(".nav-bar").slideDown();
+			$(".nav-bar").css({'display': 'block'});
+			$(".nav-bar").addClass("activo");
+		}
+	});
 });
